6fdc2d75504c2c35f675a30b682bc48b9211c9d6
Shaniece S <91788576+NoireMing@users.noreply.github.com>
2024-01-09 20:31:25 -0600

    Add files via upload

---
+++ RewardValueTests.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+        int double cashValue = 100;
+        var milesValue = 10000;
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        var milesValue = 10000;
+        int cashValue = 100;
+        assertEquals(milesValue, rewardValue.getCashValue());
+    }
+}
